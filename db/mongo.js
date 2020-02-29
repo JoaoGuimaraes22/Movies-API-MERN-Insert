f9ef7deeb58fe44b94f98813b0438ab177a44fd0
JoaoGuimaraes22 <jssgmrs22@gmail.com>
2020-02-29 00:25:34 +0000

    Minor text change

--- db/mongo.js
+++ db/mongo.js
@@ -19,9 +19,9 @@ const mongoMovies = () => {
 
       // Insert a single document
       const data = await movies();
-      const col = await db.collection("inserts").drop().catch((err) => { console.log(`${err}, or there was no inserts collection`) });
+      const col = await db.collection("movies").drop().catch((err) => { console.log(`${err}, or there was no movies collection`) });
       data.forEach(async (el) => {
-        let r = await db.collection('inserts').insertOne({ title: el.title, releaseDate: el.releaseDate, img: el.img, desc: el.desc, rating: el.rating });
+        let r = await db.collection('movies').insertOne({ title: el.title, releaseDate: el.releaseDate, img: el.img, desc: el.desc, rating: el.rating });
         assert.equal(1, r.insertedCount);
       })
     }
